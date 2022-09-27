814605b55ff4bb3ab13c62a64f4981a32e1ed276
alex <mendelee23@bk.ru>
2022-09-27 19:53:28 +0300

    Create: dataclass ShopRepositoryImplementation

---
+++ app/src/main/java/com/example/shoppinglist/data/ShopListRepositoryImpl.kt
@@ -0,0 +1,39 @@
+package com.example.shoppinglist.data
+
+import com.example.shoppinglist.domain.ShopItem
+import com.example.shoppinglist.domain.ShopListRepository
+
+class ShopListRepositoryImpl: ShopListRepository {
+
+    private val shopList = mutableListOf<ShopItem>()
+
+    private var autoIncrementId = 0
+
+    override fun getShopItem(shopitemId: Int): ShopItem {
+        return shopList.find {
+            it.id==shopitemId
+        } ?: throw RuntimeException("Element with id $shopitemId not found")
+    }
+
+    override fun addShopItem(shopItem: ShopItem) {
+        if (shopItem.id == ShopItem.UNDEFINED_ID){
+            shopItem.id = autoIncrementId++
+        }
+        shopList.add(shopItem)
+    }
+
+    override fun deleteShopItem(shopItem: ShopItem) {
+        shopList.remove(shopItem)
+    }
+
+    override fun editShopItem(shopItem: ShopItem) {
+        val oldElement = getShopItem(shopItem.id)
+        shopList.remove(oldElement)
+        addShopItem(shopItem)
+    }
+
+    override fun getShopList(): List<ShopItem> {
+        return shopList.toMutableList()
+    }
+
+}
